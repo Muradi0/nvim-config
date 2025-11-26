96460f684a2b4a38b1c612dea7c300f853f598dc
Volodymyr Filippov <filqwerty987@gmail.com>
2025-11-26 23:00:49 +0100

    fix dublicate lsp problem

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -6,6 +6,7 @@ return {
 			require("mason").setup()
 		end,
 	},
+
 	{
 		"williamboman/mason-lspconfig.nvim",
 		lazy = false,
@@ -13,84 +14,6 @@ return {
 			auto_install = true,
 		},
 	},
-
-	-- {
-	-- 	"neovim/nvim-lspconfig",
-	-- 	lazy = false,
-	-- 	config = function()
-	-- 		local lspconfig = require("lspconfig")
-	-- 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-	--
-	-- 		vim.lsp.config("lua_ls", {
-	-- 			capabilities = capabilities,
-	-- 		})
-	--
-	-- 		vim.lsp.config("basedpyright", {
-	-- 			capabilities = capabilities,
-	-- 		})
-	--
-	-- 		vim.lsp.config("gopls", {
-	-- 			capabilities = capabilities,
-	-- 		})
-	--
-	-- 		vim.lsp.config("jdtls", {
-	-- 			capabilities = capabilities,
-	-- 		})
-	--
-	-- 		vim.lsp.config("tailwindcss", {
-	-- 			capabilities = capabilities,
-	-- 		})
-	--
-	-- 		vim.lsp.config("jsonls", {
-	-- 			capabilities = capabilities,
-	-- 		})
-	--
-	-- 		vim.lsp.config("eslint", {
-	-- 			capabilities = capabilities,
-	-- 		})
-	--
-	-- 		-- vim.lsp.config("tsserver", {
-	-- 		-- 	capabilities = capabilities,
-	-- 		-- })
-	-- 		--
-	-- 		-- lspconfig.tsserver.setup({
-	-- 		-- 	capabilities = capabilities,
-	-- 		-- })
-	--
-	-- 		local mason_registry = require("mason-registry")
-	-- 		local vue_language_server = mason_registry.get_package("vue-language-server"):get_install_path()
-	-- 			.. "/node_modules/@vue/language-server"
-	--
-	-- 		lspconfig.ts_ls.setup({
-	-- 			capabilities = capabilities,
-	-- 			init_options = {
-	-- 				plugins = {
-	-- 					{
-	-- 						name = "@vue/typescript-plugin",
-	-- 						location = vue_language_server,
-	-- 						languages = { "vue" },
-	-- 					},
-	-- 				},
-	-- 			},
-	-- 			filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-	-- 		})
-	--
-	-- 		vim.lsp.enable("lua_ls")
-	-- 		vim.lsp.enable("basedpyright")
-	-- 		vim.lsp.enable("gopls")
-	-- 		vim.lsp.enable("jdtls")
-	-- 		vim.lsp.enable("tsserver")
-	-- 		vim.lsp.enable("tailwindcss")
-	-- 		vim.lsp.enable("jsonls")
-	-- 		vim.lsp.enable("eslint")
-	--
-	-- 		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-	-- 		vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-	-- 		vim.keymap.set("n", "<leader>grr", vim.lsp.buf.references, {})
-	-- 		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-	-- 		vim.keymap.set("n", "<leader>grn", vim.lsp.buf.rename, {})
-	-- 	end,
-	-- },
 
 	{
 		"neovim/nvim-lspconfig",
@@ -152,11 +75,11 @@ return {
 
 			-- If you are on most recent `nvim-lspconfig`
 			local vue_ls_config = {}
-
-			vim.lsp.config("vtsls", vtsls_config)
+			--
+			-- vim.lsp.config("vtsls", vtsls_config)
 			vim.lsp.config("vue_ls", vue_ls_config)
 			vim.lsp.config("ts_ls", ts_ls_config)
-			vim.lsp.enable({ "vtsls", "vue_ls" })
+			vim.lsp.enable({ "ts_ls", "vue_ls" })
 
 			-- 4. Keymaps (Global LSP mappings)
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
